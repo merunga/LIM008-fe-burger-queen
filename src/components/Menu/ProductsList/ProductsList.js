85ae617fe7c410097ed55e1af96c1439df12d984
lmyEsther <esthercheremo@gmail.com>
2019-04-05 18:42:53 -0500

    configuracion de eslint

--- src/components/Menu/ProductsList/ProductsList.js
+++ src/components/Menu/ProductsList/ProductsList.js
@@ -1,53 +1,49 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 import Product from './Product/Product';
 import db from '../../../services/firestore';
 
+const ProductsList = (props) => {
 
-// const description = [
-//     {id: 'cafeA', label: 'Café Americano', price: 3},
-//     {id: 'hamburS', label: 'Hamburguesa Simple', price: 4},
-//     {id: 'aguaP', label: 'Agua 500ml', price: 5}
-// ];
+  const [diningData, setDiningData] = useState([]);
 
-const ProductsList = ( props ) => {
+  const { __, __, value } = useCollection(
+    db.collection('dining'),
+  );
+  setDiningData(value);
+  // useEffect(async () => {
+  //   const result = await db.collection('/dining').get();
+  //   const data = await {id: result.id, ...result.data()};
+  //   return setDiningData(data);
 
-    const [diningData, setDiningData] = useState([]);
-
-  useEffect(async () => {
-    const result = await db.collection('/dining').get();
-    const data = await {id: result.id, ...result.data()};
-    return setDiningData(data);
-      
-    //   return db.collection('/dining').onSnapshot(query => {
-    //     setDiningData(query.docs.map(d => d.data()) );
-    //   });
+  //   //   return db.collection('/dining').onSnapshot(query => {
+  //   //     setDiningData(query.docs.map(d => d.data()) );
+  //   //   });
 
 
-    //   return db.collection('/dining').onSnapshot((query) => {
-    //     const data = [];
-    //     query.forEach(doc => { data.push({id: doc.id, ...doc.data()}) })
-    //   setDiningData(data);
-    //   console.log(data);
-    //   debugger;
+  //   //   return db.collection('/dining').onSnapshot((query) => {
+  //   //     const data = [];
+  //   //     query.forEach(doc => { data.push({id: doc.id, ...doc.data()}) })
+  //   //   setDiningData(data);
+  //   //   console.log(data);
+  //   //   debugger;
 
-  }, []);
+  // }, [diningData]);
 
-
-
-
-    return (
-    <div>
-        {diningData.map(din => (
-            <Product 
-            key={din.label} 
-            label={din.label}
-            price={din.price}
-            added={() => props.addedProduct(din.id)}
-            removed={() => props.removedProduct(din.id)} />
+  return (
+      <div>
+      {diningData.map(din => (
+          <Product
+              key={din.label}
+              label={din.label}
+              price={din.price}
+              added={() => props.addedProduct(din.id)}
+              removed={() => props.removedProduct(din.id)}
+            />
         ))}
     </div>
-    )
+  );
 };
 
 export default ProductsList;
