30f9b4ef9a8dfed362ba9e8ed953b2a5a1542f70
cinthyasegura <cinthyaless@gmail.com>
2019-04-06 11:47:30 -0500

    se usa useEffect para obtener la data del json

--- src/App.js
+++ src/App.js
@@ -1,39 +1,62 @@
-import React, { Component } from 'react';
-import User from './components/user';
-import Header from './components/layout/Header';
-import Tabs from './components/layout/Tabs';
-import ProductList from './components/ProductList';
-import Amount from './components/order';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import User from './components/user';
+// import Header from './components/layout/Header';
+// import Tabs from './components/layout/Tabs';
+// import ProductList from './components/ProductList';
+// import Amount from './components/order';
 
-class App extends Component {
-  state = {
-    menu: [],
-    filter: ''
-  };
-  componentDidMount() {
+
+
+function App() {
+  const [menu, setMenu] = useState([]);
+  // const [filter] = useState('');
+  useEffect(() => {
     fetch('https://raw.githubusercontent.com/cinthyasegura/LIM008-fe-burger-queen/2ba3df5cbef051f1557745e2e24645d44a33375e/src/data/menu.json')
-     .then(resp => resp.json())
-     .then(json =>this.setState({ menu: json, filter: 'breakfast' }))
-  }
-  filteredList = filter => {
-    return this.setState({filter: filter})
-  }
+    .then(resp => resp.json())
+    .then(data => {
+      setMenu(menu.concat(data))  
+    }) 
+  }, []);
 
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Tabs filtered={this.state.filter} onclick={this.filteredList} />
-        <ProductList menu={this.state.menu.filter(item => item.category === this.state.filter)}/>
-        <User />
-        <Amount />
-        
-      </div>
-    )
-  }
+
+  return (
+          <div>
+            {menu.map(item => <p key={item.id}>{item.name}</p>)}
+          </div>
+     
+        )
 }
+
+
+// class App extends Component {
+
+//   state = {
+//     menu: [],
+//     filter: ''
+//   };
+//   componentDidMount() {
+    
+//   }
+//   filteredList = filter => {
+//     return this.setState({filter: filter})
+//   }
+
+
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         <Tabs filtered={this.state.filter} onclick={this.filteredList} />
+//         <ProductList menu={this.state.menu.filter(item => item.category === this.state.filter)}/>
+//         <User />
+//         <Amount />
+        
+//       </div>
+//     )
+//   }
+// }
 
 export default App
 
