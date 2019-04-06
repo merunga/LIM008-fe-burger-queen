6df00a805ee4ade6ecc2d5bbdc6cd48cc9b8db39
cinthyasegura <cinthyaless@gmail.com>
2019-04-06 12:24:19 -0500

    filtrado usando hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-// import User from './components/user';
-// import Header from './components/layout/Header';
-// import Tabs from './components/layout/Tabs';
-// import ProductList from './components/ProductList';
+import User from './components/user';
+import Header from './components/layout/Header';
+import Tabs from './components/layout/Tabs';
+import ProductList from './components/ProductList';
 // import Amount from './components/order';
 
 
 
 function App() {
   const [menu, setMenu] = useState([]);
+  let [options, setOptions] = useState('');
   // const [filter] = useState('');
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/cinthyasegura/LIM008-fe-burger-queen/2ba3df5cbef051f1557745e2e24645d44a33375e/src/data/menu.json')
     .then(resp => resp.json())
     .then(data => {
       setMenu(menu.concat(data))  
+      setOptions(options = 'breakfast')
     }) 
   }, []);
 
-
-
+  const filteredList = filter => {
+    return setOptions(options = filter);
+   }
   return (
           <div>
-            {menu.map(item => <p key={item.id}>{item.name}</p>)}
+            <Header />
+            <Tabs filtered={options} onclick={filteredList} />
+           <ProductList menu={menu.filter(item => item.category === options)}/>
+            <User />
           </div>
      
         )
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 
 const ProductList = ({ menu }) => {
-   console.log(menu);
-   
+ 
     return (
        menu.map(item => <p key={item.id}>{item.name}</p>)
     )
