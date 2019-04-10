7f123230837285d7654b5e21d65e96055ae1f9e9
cinthyasegura <cinthyaless@gmail.com>
2019-04-10 16:59:58 -0500

    cambios en css

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect} from 'react';
-// import User from './User';
 import Tabs from './layout/Tabs';
 import ProductList from './ProductList';
 import OrderSummary from './OrderSummary';
@@ -13,61 +12,65 @@ const Container = () => {
     let [clientsName, setClientsName] = useState('');
   
     useEffect(() => {
-      async function fetchDta() {
-        const result = await axios('https://raw.githubusercontent.com/cinthyasegura/LIM008-fe-burger-queen/firstHistory/src/data/menu.json');
-        setMenu([...result.data, menu])
-        setOptions(options = 'breakfast')
-      };
-    fetchDta()
+        async function fetchDta() {
+            const result = await axios('https://raw.githubusercontent.com/cinthyasegura/LIM008-fe-burger-queen/firstHistory/src/data/menu.json');
+            setMenu([...result.data, menu]);
+            setOptions(options = 'breakfast');
+        }
+        fetchDta();
     }, []);
 
     const matchOption = option => setOptions(options = option);
 
     const addOrderItem = id => {
-      return menu.filter(item => item.id === id ? setOrderItems([...orderItems, item]) : '')
+        return menu.filter(item => item.id === id ? setOrderItems([...orderItems, item]) : '');
     };
     
 
     const deleteItem = id => {
-      setOrderItems(orderItems.filter(item => item.id !== id))
+        setOrderItems(orderItems.filter(item => item.id !== id));
     };
     
     const updateItem = (index, item) => {
-      const newItems = [...orderItems];
-      newItems[index] = item;
-      setOrderItems(newItems)
-
+        const newItems = [...orderItems];
+        newItems[index] = item;
+        setOrderItems(newItems);
     };
 
     const updateInput = e => {
-      setClientsName(clientsName = e.target.value)
-    }
+        setClientsName(clientsName = e.target.value);
+    };
  
     const addUser = e => {
-      e.preventDefault();
-      const db = firebase.firestore();
-      db.collection('users').add({clientsName, orderItems});
-      setClientsName(clientsName = '')
-    }
-    
+        e.preventDefault();
+        const db = firebase.firestore();
+        db.collection('users').add({clientsName, orderItems});
+        setClientsName(clientsName = '');
+    };
     
     return (
-      <div>
-        <Tabs matchOption={matchOption} />
-        <ProductList menu={menu.filter(item => item.category === options)} addOrderItem={addOrderItem} />
-        <OrderSummary orderItems={orderItems} deleteItem={deleteItem} updateItem={updateItem} />
-        <form onSubmit={addUser}>
-            <input
-              type="text"
-              name="name"
-              placeholder="Nombre del cliente"
-              onChange={updateInput}
-              value={clientsName}
-            />
-            <button type="submit">Enviar a cocina</button>
-          </form>
-      </div>
-    )
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <Tabs matchOption={matchOption} />
+                    <ProductList menu={menu.filter(item => item.category === options)} addOrderItem={addOrderItem} />
+                </div>
+                <div className="col-md-6">
+                    <OrderSummary orderItems={orderItems} deleteItem={deleteItem} updateItem={updateItem} />
+                    <form onSubmit={addUser}>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Nombre del cliente"
+                            onChange={updateInput}
+                            value={clientsName}
+                        />
+                        <button className="rounded bg-info text-light" type="submit">Enviar a cocina</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Container;
--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -2,54 +2,54 @@ import React from 'react';
 import './layout/layout.css';
 
 const OrderSummary = ({ orderItems, deleteItem, updateItem }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Cantidad</th>
-          <th>Producto</th>
-          <th>Precio</th>
-          <th>Eliminar</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orderItems.length > 0 ? (
-          orderItems.map((item, index) => (
-            <tr key={index}>
-              <td>
-                <button onClick={() => {
-                  const newItemToAdd = { ...item };
-                  newItemToAdd.quantity += 1
-                  updateItem(index, newItemToAdd);
-                }}> + </button>
-                <p>{item.quantity}</p>
-                <button onClick={() => {
-                  const newItemToDecrease = { ...item };
-                  newItemToDecrease.quantity -=1
-                  updateItem(index, newItemToDecrease);
-                }}> - </button> 
-              </td>
-              <td>{item.name}</td>
-              <td><span>$</span>{item.price*item.quantity}</td>
-              <td>
-              <i className="far fa-trash-alt pointer" onClick={() => deleteItem(item.id)}></i>
-              </td>
-            </tr>
-          ))
-        ) : (
-          <tr>
-            <td>Sin orden</td>
-          </tr>
-        )}
-      </tbody>
-      <tfoot>
-        <tr>
-          <th colSpan="3">Total:</th>
-          <td>{orderItems.reduce((acum, element) => acum + (element.quantity*element.price), 0)} </td>
-        </tr>
-      </tfoot>
-    </table>
-  )
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="d-inline p-5">Cantidad</th>
+                    <th className="d-inline p-5">Producto</th>
+                    <th className="d-inline p-5">Precio</th>
+                    <th className="d-inline">Eliminar</th>
+                </tr>
+            </thead>
+            <tbody>
+                {orderItems.length > 0 ? (
+                    orderItems.map((item, index) => (
+                        <tr key={index}>
+                            <td>
+                                <button onClick={() => {
+                                    const newItemToAdd = { ...item };
+                                    newItemToAdd.quantity += 1;
+                                    updateItem(index, newItemToAdd);
+                                }}> + </button>
+                                <p>{item.quantity}</p>
+                                <button onClick={() => {
+                                    const newItemToDecrease = { ...item };
+                                    newItemToDecrease.quantity -=1;
+                                    updateItem(index, newItemToDecrease);
+                                }}> - </button> 
+                            </td>
+                            <td>{item.name}</td>
+                            <td><span>$</span>{item.price*item.quantity}</td>
+                            <td>
+                                <i className="far fa-trash-alt pointer" onClick={() => deleteItem(item.id)}></i>
+                            </td>
+                        </tr>
+                    ))
+                ) : (
+                    <tr>
+                        <td>Sin orden</td>
+                    </tr>
+                )}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th colSpan="3">Total:</th>
+                    <td>$ {orderItems.reduce((acum, element) => acum + (element.quantity*element.price), 0)} </td>
+                </tr>
+            </tfoot>
+        </table>
+    );
 };
 
 export default OrderSummary;
